c8870c1854f3cd6800a748e2c1247f9b1de17d85
Nakarin.Vi <nakarin.v@arcadiaapm.com>
2022-03-15 12:35:14 +0700

    Add Headless and Language in Regression.bat

--- Regression.bat
+++ Regression.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 TITLE AIS Andromeda regression test
-robot -d "Regression" -v "headless:True" -e "On-Hold" TestSuite
+robot -d "Regression" -v "HEAD_LESS:True" -v "LANG:EN" -v "TEST_SITE:DEV" -e "On-Hold" TestSuite
 PAUSE
